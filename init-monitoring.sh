0fa01f1629c9c1bc936064d72f5fd6dccd38f6eb
Michael Oh <mike.js.oh@gmail.com>
2023-01-07 15:52:56 -0500

    initial script

---
+++ init-monitoring.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+apt update
+mkdir -p /etc/prometheus
+mkdir -p /var/lib/prometheus
+wget https://github.com/prometheus/prometheus/releases/download/v2.31.0/prometheus-2.31.0.linux-amd64.tar.gz
+tar -xvf prometheus-2.31.3.linux-amd64.tar.gz
+cd prometheus-*
+mv prometheus promtool /usr/local/bin/
+mv consoles/ console_libraries/ /etc/prometheus/
+mv prometheus.yml /etc/prometheus/prometheus.yml
+groupadd --system prometheus
+useradd -s /sbin/nologin --system -g prometheus prometheus
+chown -R prometheus:prometheus /etc/prometheus/ /var/lib/prometheus/
+chmod -R 775 /etc/prometheus/ /var/lib/prometheus/
+
+touch /etc/systemd/system/prometheus.service
+cd /etc/systemd/system/
+tee -a prometheus.service << END
+[Unit]
+Description=Prometheus
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+User=prometheus
+Group=prometheus
+Restart=always
+Type=simple
+ExecStart=/usr/local/bin/prometheus \
+    --config.file=/etc/prometheus/prometheus.yml \
+    --storage.tsdb.path=/var/lib/prometheus/ \
+    --web.console.templates=/etc/prometheus/consoles \
+    --web.console.libraries=/etc/prometheus/console_libraries \
+    --web.listen-address=0.0.0.0:9090
+
+[Install]
+WantedBy=multi-user.target
+END
+
+systemctl start prometheus
+systemctl enable prometheus
+systemctl status prometheus
